c38c6919d9d86c00d01b122bd243928c992aec8a
Mohammad Noh Iskandar Jamaluddin <anatraxia@gmail.com>
2017-07-23 14:04:38 +0800

    added firebase.config and change index.js and index.html for the css

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyCXyqX9Qc5Q72HsJautwZnj95nExggHQm4",
+    authDomain: "fun-food-friends-ea68d.firebaseapp.com",
+    databaseURL: "https://fun-food-friends-ea68d.firebaseio.com",
+    projectId: "fun-food-friends-ea68d",
+    storageBucket: "fun-food-friends-ea68d.appspot.com",
+    messagingSenderId: "955830005424"
+  };
+firebase.initializeApp(config);
+export default firebase;
